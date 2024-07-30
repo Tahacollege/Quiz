eb36770e613edc1f9df2fee690714e797754c737
Taha <110016145+Tahacollege@users.noreply.github.com>
2024-07-30 08:19:01 +0530

    mongo changes

--- db.js
+++ db.js
@@ -1,5 +1,6 @@
 const {MongoClient}=require('mongodb')
-const url='mongodb://localhost:27017'
+//const url='mongodb://localhost:27017'
+const url="mongodb+srv://tcollegewala30:fOcy87YhffoTWgnJ@cluster0.qqtdpgf.mongodb.net/b.net/"
 const database='QuizOrbit'
 async function DbConnection(){
     const client=new MongoClient(url)
